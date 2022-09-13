48d338e29c16b8a15b3761d0e6418fd712677543
jorge <jorge@garlicoin.io>
2022-09-12 20:38:08 -0400

    Testgen readme and comment fixes

--- contrib/testgen/gen_key_io_test_vectors.py
+++ contrib/testgen/gen_key_io_test_vectors.py
@@ -6,8 +6,8 @@
 Generate valid and invalid base58 address and private key test vectors.
 
 Usage:
-    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py valid 50 > ../../src/test/data/key_io_valid.json
-    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py invalid 50 > ../../src/test/data/key_io_invalid.json
+    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py valid 50 > ../../src/test/data/base58_keys_valid.json
+    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py invalid 50 > ../../src/test/data/base58_keys_invalid.json
 '''
 # 2012 Wladimir J. van der Laan
 # Released under MIT License
